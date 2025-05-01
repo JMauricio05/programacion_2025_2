9f101cc962ae23967af5f3ed003a286090a740da
SrProfesorDonDiego <164379827+SrProfesorDonDiego@users.noreply.github.com>
2025-04-30 19:07:38 -0500

    Ejemplo de api 2

---
+++ ejemplo_ms/app/Models/ProgramacionTema.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProgramacionTema extends Model
+{
+    protected $table = "programacion_temas";
+    public $timestamps = false;
+}
