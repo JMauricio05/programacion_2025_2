768d27bc563efebd1096c8264bcdf2ed9be37818
SrProfesorDonDiego <164379827+SrProfesorDonDiego@users.noreply.github.com>
2025-04-30 19:30:39 -0500

    Configuración de endpoints

--- ejemplo_ms/routes/api.php
+++ ejemplo_ms/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\ProgramacionTemaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,12 @@ Route::controller(PersonaController::class)->group(function () {
     Route::get('persona/{id}', 'show');
     Route::put('persona/{id}', 'update');
     Route::delete('persona/{id}', 'destroy');
+});
+
+Route::controller(ProgramacionTemaController::class)->group(function () {
+    Route::get('temas', 'index');
+    Route::post('tema', 'store');
+    Route::get('tema/{id}', 'show');
+    Route::put('tema/{id}', 'update');
+    Route::delete('tema/{id}', 'destroy');
 });
